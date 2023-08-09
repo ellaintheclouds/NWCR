9962e4ec7aae3dafbb07e799f95314e39c505788
ellaintheclouds <138864301+ellaintheclouds@users.noreply.github.com>
2023-08-09 13:21:09 +0100

    Add files via upload
    The ability to download plots as both .zip of .png, and as a singular .pdf are both working.

--- app.R
+++ app.R
@@ -18,8 +18,6 @@ if (!require("plotly", quietly = TRUE)) { install.packages("plotly") }
 if (!require("viridis", quietly = TRUE)) { install.packages("viridis") } 
 if (!require("zip", quietly = TRUE)) { install.packages("zip") } 
 
-#devtools::install_github('ropensci/plotly')
-
 # General
 library(shiny)
 library(stringr) # Makes working with strings simpler.
@@ -39,7 +37,7 @@ library(factoextra) # For principal component analysis
 library(ggfortify) # Allows plotting of PCA and survival analysis.
 library(umap) # Algorithm for dimensional reduction
 
-## Data ##
+# Data----------
 source("PCA_function.R")
 
 list_of_cancer_types <- c("Acute Myeloid Leukemia" = "TCGA-LAML",                                                                                
@@ -75,28 +73,39 @@ ui <- fluidPage(
   
   titlePanel("TCGA Dummy App"),
   
+  # Sidebar----------
   sidebarLayout(
     sidebarPanel(
+      # Cancer type input
       checkboxGroupInput("cancer_type_list", "Select Cancer Type", list_of_cancer_types),
       br(), 
       
+      # Gene input
       print(HTML("<strong>Input gene names</strong> <br/> use Ensembl format and separate by new lines")), 
       textAreaInput("gene_string", "", rows = 5), 
       
+      # Button that triggers PCA computation
       actionButton("button_pca_analysis", "Principal Component Analysis"), 
       
     ), # Sidebar panel
     
+    # Main panel----------
     mainPanel(
+      # Drop-down menus that reactively alter display PCA plot
       uiOutput("cancer_type_menu"), 
       uiOutput("gene_menu"), 
       uiOutput("pcx_menu"), 
       uiOutput("pcy_menu"), 
 
+      # Display PCA plot
       plotlyOutput("display_plot", width = "100%",
                    height = "1000px"), 
       
-      uiOutput("download_button_object") #------------------------------------------------------------------------
+      # Download buttons
+      uiOutput("download_pdf_button"), 
+      uiOutput("download_zip_button")
+
+      
     ) # Main panel
   ) # Sidebar layout
 ) # Fluid page
@@ -139,10 +148,12 @@ server <- function(input, output) {
     }) # Render Plotly
     
     # Download all plots-------------------------------------------------------------------------------------------
-    output$download_button_object <- renderUI(downloadButton("download_button", "Download All PCA Plots"))
+    # Reactive download buttons
+    output$download_zip_button <- renderUI(downloadButton("download_zip", "Download All Plot Images"))
+    output$download_pdf_button <- renderUI(downloadButton("download_pdf", "Download All Plots as a PDF"))
     
-    # create zip
-    output$download_button <-downloadHandler(
+    # Create .zip with individual .png plots
+    output$download_zip <-downloadHandler(
       filename = function(){"out.zip"},
       content = function(file){
         if(TRUE){
@@ -166,22 +177,20 @@ server <- function(input, output) {
         }
 
         zip::zip(zipfile = file, files=all_files)
-        #write.csv(data.frame(x=1:3, y=1:3), "test.csv")
-        #zip("out.zip", "test.csv")
-        #file.copy("out.zip", file)
       }, # Content function
       
       contentType = "application/zip"
       ) # Download handler
     
     
-    #output$download_button = downloadHandler(
-    #  filename = function() {"plots.pdf"}, 
-    #  content = function(file) {
-    #    pdf(file, onefile = TRUE, width = 15, height = 10)
-    #    for(plot_printout in out_plots){ print(plot_printout)}
-    #    dev.off()
-    #  })                      
+    # Create singular .pdf containing all plots
+    output$download_pdf = downloadHandler(
+      filename = function() {"plots.pdf"}, 
+      content = function(file) {
+        pdf(file, onefile = TRUE, width = 15, height = 10)
+        for(plot_printout in out_plots){ print(plot_printout)}
+        dev.off()
+      })                      
     
   }) # Observe event
 } # Server
