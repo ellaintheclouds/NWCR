d8dede8b67e112525b0f6057ef59d6aaae6a6584
ellaintheclouds <138864301+ellaintheclouds@users.noreply.github.com>
2023-08-10 14:16:07 +0100

    Add files via upload
    The PCA function now has been updated so that out_plots is now output_data, and output_data has another first level, one of which contains scree plots.

--- PCA_function.R
+++ PCA_function.R
@@ -61,30 +61,30 @@ for(current_name in names(list_of_cancer_types)){
 }
 
 
-
 #1 Start of function------------------------------------------------------------
 pca_function <- function(cancer_type_list, gene_string){
   gene_list <- strsplit(gene_string, split = "\n")[[1]] # Split the user input into a list
   
   # storing all plots
-  out_plots <- list()
+  output_data <- list("pca plots", "contribution dataframes")
+  output_data[["contribution dataframes"]] <- list()
+  output_data[["pca_plots"]][[current_cancer_type]] <- list()
   
-#2 Formatting and looping through cancer type data------------------------------
-   for(current_cancer_type in cancer_type_list){
+  
+  #2 Formatting and looping through cancer type data------------------------------
+  for(current_cancer_type in cancer_type_list){
     
-     # Re-assigning names to cancer types
-     current_cancer_type_named <- current_cancer_type 
-     names(current_cancer_type) <- list_of_cancer_types_rev[current_cancer_type]
-     
+    # Re-assigning names to cancer types
+    current_cancer_type_named <- current_cancer_type 
+    names(current_cancer_type) <- list_of_cancer_types_rev[current_cancer_type]
+    
     current_file_name <- paste0(current_cancer_type, ".rds")
     print(current_file_name) # Prints the names of the files that are being accessed in the loop
     
-    out_plots[[current_cancer_type]] <- list()
-    
     #data_RNAseq <- readRDS(current_file_name) # Reads in the file to be used in this iteration of the loop
     data_RNAseq <- readRDS(paste0("data/", current_file_name))  
-
-  #3 Get the count data and the clinical annotation-------------------------------
+    
+    #3 Get the count data and the clinical annotation-------------------------------
     count_data <- assay(data_RNAseq, "unstranded") # gene count matrix
     tpm_matrix <- assay(data_RNAseq, "tpm_unstrand") # transcript per million matrix (for plotting colour later)
     
@@ -126,11 +126,13 @@ pca_function <- function(cancer_type_list, gene_string){
     pca_positions <- as.data.frame(count_pca$x)
     pca_summary <- summary(count_pca)$importance[2,] * 100 # Showing the contribution of each position, to represent its importance.----------------------------------------------------------------------------------------------------------------------------------------------------
     
-
+    output_data[["contribution dataframes"]][[current_cancer_type]] <- contribution_dataframe
+    
+    
     #6 Formatting and looping through gene data-------------------------------------
     for(current_gene in gene_list){
       
-      out_plots[[current_cancer_type]][[current_gene]] <- list()
+      output_data[["pca plots"]][[current_cancer_type]][[current_gene]] <- list()
       
       tpm_dataframe <- as.data.frame(t(tpm_matrix))
       tpm_dataframe$sample_id_tpm <- rownames(tpm_dataframe)
@@ -140,71 +142,71 @@ pca_function <- function(cancer_type_list, gene_string){
       sample_annotation_tpm <- merge(sample_annotation, tpm_dataframe, by = 0)
       row.names(sample_annotation_tpm) <- sample_annotation_tpm$sample_id_tpm
       sample_annotation_tpm$Row.names <- NULL
-    
+      
       # Gene relationships
       pca_plot_df <- as.data.frame(merge(sample_annotation_tpm, as.data.frame(count_pca$x), by.x= 0, by.y= 0))
-        
-        
+      
+      
       #7 Start plot function for every possible combination of dimensions-------------
-        for(pcx in 1:10){
-          for(pcy in 1:10){
+      for(pcx in 1:10){
+        for(pcy in 1:10){
+          
+          
+          #8 Plotting PCA-----------------------------------------------------------------
+          # the if statement is to remove duplicate plots (i.e. it will just plot PC3 vs PC4, and not PC4 vs PC3)
+          if(pcx < pcy){
+            plotting_dimentions <- c(pcx, pcy)
             
+            ## Find top contributing genes for pcx
+            top_contrib_genes_x <- data.frame(contribution = gene_contribution[,pcx], 
+                                              gene_contribution_percentile = gene_contribution_percentile[,pcx], 
+                                              gene = row.names(gene_contribution))
+            # order by contribution percentile
+            top_contrib_genes_x <- top_contrib_genes_x[order(top_contrib_genes_x$gene_contribution_percentile),]
+            print(paste0("Top contributing genes for ", pcx, ":"))
+            print(head(top_contrib_genes_x))
             
-      #8 Plotting PCA-----------------------------------------------------------------
-            # the if statement is to remove duplicate plots (i.e. it will just plot PC3 vs PC4, and not PC4 vs PC3)
-            if(pcx < pcy){
-              plotting_dimentions <- c(pcx, pcy)
-              
-              ## Find top contributing genes for pcx
-              top_contrib_genes_x <- data.frame(contribution = gene_contribution[,pcx], 
-                                                gene_contribution_percentile = gene_contribution_percentile[,pcx], 
-                                                gene = row.names(gene_contribution))
-              # order by contribution percentile
-              top_contrib_genes_x <- top_contrib_genes_x[order(top_contrib_genes_x$gene_contribution_percentile),]
-              print(paste0("Top contributing genes for ", pcx, ":"))
-              print(head(top_contrib_genes_x))
-              
-              ## Find top contributing genes for pcy
-              top_contrib_genes_y <- data.frame(contribution = gene_contribution[,pcy], 
-                                                gene_contribution_percentile = gene_contribution_percentile[,pcy], 
-                                                gene = row.names(gene_contribution))
-              # order by contribution percentile
-              top_contrib_genes_y <- top_contrib_genes_y[order(top_contrib_genes_y$gene_contribution_percentile),]
-              print(paste0("Top contributing gene for ", pcy, ":"))
-              print(head(top_contrib_genes_y))
-              
-              ## plot PCA
-              current_pca_plot_df <- pca_plot_df[,c("sample_submitter_id", "tpm", paste0("PC", pcx), paste0("PC", pcy))]
-              colnames(current_pca_plot_df) <- c("sample_submitter_id", "tpm", "pcx", "pcy")
-              
-              # Labels for the axis
-              tpm_label <- "Transcripts\n per\n Million"
-              
-              title_label <- paste0(current_gene, " Expression in ", names(current_cancer_type))
-              
-              # Plot
-              pca_plot <- ggplot(data = current_pca_plot_df, aes(x = pcx, y = pcy)) + 
-                geom_point(aes(colour = tpm)) +
-                labs(title = title_label, colour = tpm_label) + 
-                theme_bw() + 
-                xlab(paste0("PC", pcx, " (", pca_summary[pcx], ")")) + 
-                ylab(paste0("PC", pcy, " (", pca_summary[pcy], ")")) + 
-                scale_colour_viridis()
-              
-              pca_plot
-              
-              out_plots[[current_cancer_type]][[current_gene]][[paste0(pcx, "_", pcy)]] <- pca_plot
-              
-              #dir.create(file.path(paste0("out/pca/", current_cancer_type)), showWarnings = FALSE)
-              #dir.create(file.path(paste0("out/pca/", current_cancer_type, "/", current_gene)), showWarnings = FALSE)
-              
-              #ggsave(paste0("out/pca/", current_cancer_type, "/", current_gene, "/PC", pcx, "_PC", pcy, ".png"), pca_plot, height = 12, width = 16, units = "cm")              
-            } # Plotting PCs when pcx < pcy
-          } # pcy
-        } # pcx
+            ## Find top contributing genes for pcy
+            top_contrib_genes_y <- data.frame(contribution = gene_contribution[,pcy], 
+                                              gene_contribution_percentile = gene_contribution_percentile[,pcy], 
+                                              gene = row.names(gene_contribution))
+            # order by contribution percentile
+            top_contrib_genes_y <- top_contrib_genes_y[order(top_contrib_genes_y$gene_contribution_percentile),]
+            print(paste0("Top contributing gene for ", pcy, ":"))
+            print(head(top_contrib_genes_y))
+            
+            ## plot PCA
+            current_pca_plot_df <- pca_plot_df[,c("sample_submitter_id", "tpm", paste0("PC", pcx), paste0("PC", pcy))]
+            colnames(current_pca_plot_df) <- c("sample_submitter_id", "tpm", "pcx", "pcy")
+            
+            # Labels for the axis
+            tpm_label <- "Transcripts\n per\n Million"
+            
+            title_label <- paste0(current_gene, " Expression in ", names(current_cancer_type))
+            
+            # Plot
+            pca_plot <- ggplot(data = current_pca_plot_df, aes(x = pcx, y = pcy)) + 
+              geom_point(aes(colour = tpm)) +
+              labs(title = title_label, colour = tpm_label) + 
+              theme_bw() + 
+              xlab(paste0("PC", pcx, " (", pca_summary[pcx], ")")) + 
+              ylab(paste0("PC", pcy, " (", pca_summary[pcy], ")")) + 
+              scale_colour_viridis()
+            
+            pca_plot
+            
+            output_data[["pca_plots"]][[current_cancer_type]][[current_gene]][[paste0(pcx, "_", pcy)]] <- pca_plot
+            
+            #dir.create(file.path(paste0("out/pca/", current_cancer_type)), showWarnings = FALSE)
+            #dir.create(file.path(paste0("out/pca/", current_cancer_type, "/", current_gene)), showWarnings = FALSE)
+            
+            #ggsave(paste0("out/pca/", current_cancer_type, "/", current_gene, "/PC", pcx, "_PC", pcy, ".png"), pca_plot, height = 12, width = 16, units = "cm")              
+          } # Plotting PCs when pcx < pcy
+        } # pcy
+      } # pcx
     } # Gene
-   } # Cancer type
+  } # Cancer type
   
-  return(out_plots)
+  return(output_data)
   
 } # Function
